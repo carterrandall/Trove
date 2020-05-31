a57a6aab9f49695bb3dbbf5a8177a2939065557b
Carter <carterrandall1975@gmail.com>
2020-05-31 08:11:31 -0600

    Initial Commit

---
+++ Trove/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trove
+//
+//  Created by Carter Randall on 2020-05-31.
+//  Copyright © 2020 Carter Randall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
